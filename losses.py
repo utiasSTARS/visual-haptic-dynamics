Fix bug in KL calculation

losses.py
@@ -11,16 +11,15 @@ def kl(mu0, cov0, mu1, cov1, eps=1e-9):
     cov0, cov1: (bs, dim, dim)
     """
     sumf = lambda x: torch.sum(x, dim=-1)
+    prodf = lambda x: torch.prod(x, dim=-1) 
 
     k = mu0.shape[1]
     var0 = torch.diagonal(cov0, dim1=-2, dim2=-1) + eps # (bs, dim)
     var1 = torch.diagonal(cov1, dim1=-2, dim2=-1) + eps # (bs, dim)
-    ivar0 = 1 / var0
-    ivar1 = 1 / var1
+    ivar1 = 1.0 / var1
 
     a = sumf(ivar1 * var0) # (bs,)
     b = sumf((mu1 - mu0)**2 * ivar1)
     c = -k
-    d = torch.log(sumf(var1) / sumf(var0))
-
+    d = torch.log(prodf(var1) / prodf(var0))
     return 0.5 * (a+b+c+d)

tests/test_kl_divergence.py
@@ -8,11 +8,11 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
 import torch
 from losses import kl
 
-def test_KL(bs=1):
-    mu0 = -torch.rand((bs,3))
-    var0 = torch.eye(3).repeat(bs,1,1)
-    mu1 = -torch.rand((bs,3))
-    var1 = torch.eye(3).repeat(bs,1,1)
+def test_KL(bs=10):
+    mu0 = torch.rand((bs,3))
+    var0 = 3*torch.eye(3).repeat(bs,1,1)
+    mu1 = torch.rand((bs,3))
+    var1 = 2.5*torch.eye(3).repeat(bs,1,1)
     
     # Test Pytorch's KL divergence
     tic = time.time()