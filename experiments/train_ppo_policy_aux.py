165c5373935b8ac23ec0baddfc32e951b6f010ce
Oliver Limoyo <oliver.limoyo@mail.mcgill.ca>
2020-06-26 18:27:41 -0400

    Setup new runs

---
+++ experiments/train_ppo_policy_aux.py
@@ -0,0 +1,141 @@
+import os, sys, inspect, time
+sys.path.append('..')
+currentdir = os.path.dirname(os.path.abspath(
+    inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(os.path.dirname(currentdir))
+os.sys.path.insert(0, parentdir + "/pixel-environments/")
+from gym_thing.gym_thing import reacher_env, pusher_env, visual_pusher_env, visual_reacher_env
+from utils import set_seed_torch, rgb2gray, FrameStack
+from args.parser import parse_ppo_args
+import gym
+from ppo import AuxPPO
+from models import AuxActorCriticCNN
+import torch 
+import numpy as np
+
+class Memory:
+    def __init__(self):
+        self.actions = []
+        self.states = []
+        self.logprobs = []
+        self.rewards = []
+        self.is_terminals = []
+    
+    def clear_memory(self):
+        del self.actions[:]
+        del self.states[:]
+        del self.logprobs[:]
+        del self.rewards[:]
+        del self.is_terminals[:]
+
+def train(args):
+    # creating environment
+    if args.is_render is not None:
+        env = gym.make(args.env_name, is_render=args.is_render, render_h=args.dim_x[0], render_w=args.dim_x[1])
+    else:
+        env = gym.make(args.env_name)
+
+    if args.architecture =="cnn":
+        env = FrameStack(env, k=args.frame_stack)
+
+    state_dim = env.observation_space.shape[0] # 24
+    action_dim = env.action_space.shape[0] # 4
+    print("State dim: {}".format(env.observation_space.shape))
+    print("Action dim: {}".format(env.action_space.shape))
+
+    if args.random_seed:
+        print("Random Seed: {}".format(args.random_seed))
+        env.seed(args.random_seed)
+        set_seed_torch(args.random_seed)  
+
+    memory = Memory()
+
+    if args.architecture == "mlp":
+        raise NotImplementedError("Auxiliary task not available for mlp")
+    elif args.architecture =="cnn":
+        actor_critic = AuxActorCriticCNN(
+            state_dim, 
+            action_dim, 
+            args.action_std, 
+            img_dim=(args.dim_x[0], args.dim_x[1], args.frame_stack * args.dim_x[2])
+            ).to(args.device)
+
+    ppo = AuxPPO(
+        args.lr, 
+        args.gamma, 
+        args.epochs, 
+        args.eps_clip, 
+        args.device,
+        actor_critic=actor_critic
+    )
+    
+    # logging variables
+    running_reward = 0
+    avg_length = 0
+    time_step = 0
+    avg_time = 0
+
+    # training loop
+    for i_episode in range(1, args.max_episodes+1):
+        state = env.reset()
+
+        tic = time.time()
+        for t in range(args.max_timesteps):
+            time_step +=1
+
+            # Running policy_old:            
+            if args.dim_x[-1] == 1:
+                state = rgb2gray(state)
+                img_min, img_max = state.min(), state.max()
+                state = (state - img_min) / (img_max - img_min)
+
+            state = state.transpose(0, 3, 1, 2)
+            state = state.reshape(-1, *state.shape[2:])[np.newaxis]
+            action = ppo.select_action(state, memory)
+
+
+            state, reward, done, _ = env.step(action)
+            
+            # Saving reward and is_terminals:
+            memory.rewards.append(reward)
+            memory.is_terminals.append(done)
+
+            # update if its time
+            if time_step % args.update_timestep == 0:
+                print("Updating policy")
+                ppo.update(memory)
+                memory.clear_memory()
+                time_step = 0
+            running_reward += reward
+            if args.render:
+                env.render()
+            if done:
+                break
+        episode_t = time.time() - tic
+
+        avg_length += t
+        avg_time += episode_t
+
+        if i_episode % args.logging_interval == 0:
+            avg_length = int(avg_length/args.logging_interval)
+            running_reward = int((running_reward/args.logging_interval))
+            avg_time = float(avg_time/args.logging_interval)
+            
+            print('Episode {} \t Avg length: {} \t Avg reward: {} \t Avg runtime per episode: {}'\
+                    .format(i_episode, avg_length, running_reward, avg_time))
+
+            if running_reward > args.solved_reward:
+                print("########## Solved! ##########")
+                break
+
+            running_reward = 0
+            avg_length = 0
+            avg_time = 0
+
+def main():
+    args = parse_ppo_args()
+    train(args)
+
+if __name__ == '__main__':
+    main()
+    
--- models.py
+++ models.py
@@ -3,31 +3,48 @@ import torch.nn as nn
 from torch.distributions import MultivariateNormal
 from networks import FullyConvEncoderVAE, FullyConvDecoderVAE
 
-class AuxActorCriticCNN(ActorCriticCNN):
-    def __init__(self, state_dim, action_dim, action_std, shared_hidden_dim=256, img_dim=(64,64,1)):
-        super(AuxActorCriticCNN, self).__init__(
-            state_dim, 
-            action_dim, 
-            action_std, 
-            shared_hidden_dim=shared_hidden_dim, 
-            img_dim=img_dim
-        )
+class ActorCriticMLP(nn.Module):
+    def __init__(self, state_dim, action_dim, action_std):
+        super(ActorCriticMLP, self).__init__()
+        # action mean range -1 to 1
+        self.actor =  nn.Sequential(
+                nn.Linear(state_dim, 64),
+                nn.Tanh(),
+                nn.Linear(64, 32),
+                nn.Tanh(),
+                nn.Linear(32, action_dim),
+                nn.Tanh()
+                )
 
-        self.decoder = FullyConvDecoderVAE(
-            input=img_dim[2],
-            latent_size=shared_hidden_dim,
-            bn=False,
-            drop=False,
-            nl=nn.Tanh(),
-            img_dim=img_dim[1],
-            output_nl=nn.Sigmoid()
-        )
+        # critic
+        self.critic = nn.Sequential(
+                nn.Linear(state_dim, 64),
+                nn.Tanh(),
+                nn.Linear(64, 32),
+                nn.Tanh(),
+                nn.Linear(32, 1)
+                )
+        self.action_var = nn.Parameter(torch.full((action_dim,), action_std*action_std))
+        
+    def forward(self):
+        raise NotImplementedError
+    
+    def act(self, state, memory):
+        action_mean = self.actor(state)
+        cov_mat = torch.diag(self.action_var)
+        
+        dist = MultivariateNormal(action_mean, cov_mat)
+        action = dist.sample()
+        action_logprob = dist.log_prob(action)
+        
+        memory.states.append(state)
+        memory.actions.append(action)
+        memory.logprobs.append(action_logprob)
 
-    def evaluate(self, state, action): 
-        hidden = self.shared_net(state)
-        action_mean = self.actor_head(hidden.detach())
-
-        state_hat = self.decoder(hidden)
+        return action.detach()
+    
+    def evaluate(self, state, action):  
+        action_mean = self.actor(state)
 
         action_var = self.action_var.expand_as(action_mean)
         cov_mat = torch.diag_embed(action_var)
@@ -36,9 +53,10 @@ class AuxActorCriticCNN(ActorCriticCNN):
         
         action_logprobs = dist.log_prob(action)
         dist_entropy = dist.entropy()
-        state_value = self.critic_head(hidden)
-        
-        return action_logprobs, torch.squeeze(state_value), dist_entropy, state_hat
+        state_value = self.critic(state)
+
+        return action_logprobs, torch.squeeze(state_value), dist_entropy
+
 
 class ActorCriticCNN(nn.Module):
     def __init__(self, state_dim, action_dim, action_std, shared_hidden_dim=256, img_dim=(64,64,1)):
@@ -104,48 +122,32 @@ class ActorCriticCNN(nn.Module):
         
         return action_logprobs, torch.squeeze(state_value), dist_entropy
 
-class ActorCriticMLP(nn.Module):
-    def __init__(self, state_dim, action_dim, action_std):
-        super(ActorCriticMLP, self).__init__()
-        # action mean range -1 to 1
-        self.actor =  nn.Sequential(
-                nn.Linear(state_dim, 64),
-                nn.Tanh(),
-                nn.Linear(64, 32),
-                nn.Tanh(),
-                nn.Linear(32, action_dim),
-                nn.Tanh()
-                )
+        
+class AuxActorCriticCNN(ActorCriticCNN):
+    def __init__(self, state_dim, action_dim, action_std, shared_hidden_dim=256, img_dim=(64,64,1)):
+        super(AuxActorCriticCNN, self).__init__(
+            state_dim, 
+            action_dim, 
+            action_std, 
+            shared_hidden_dim=shared_hidden_dim, 
+            img_dim=img_dim
+        )
 
-        # critic
-        self.critic = nn.Sequential(
-                nn.Linear(state_dim, 64),
-                nn.Tanh(),
-                nn.Linear(64, 32),
-                nn.Tanh(),
-                nn.Linear(32, 1)
-                )
-        self.action_var = nn.Parameter(torch.full((action_dim,), action_std*action_std))
-        
-    def forward(self):
-        raise NotImplementedError
-    
-    def act(self, state, memory):
-        action_mean = self.actor(state)
-        cov_mat = torch.diag(self.action_var)
-        
-        dist = MultivariateNormal(action_mean, cov_mat)
-        action = dist.sample()
-        action_logprob = dist.log_prob(action)
-        
-        memory.states.append(state)
-        memory.actions.append(action)
-        memory.logprobs.append(action_logprob)
+        self.decoder = FullyConvDecoderVAE(
+            input=img_dim[2],
+            latent_size=shared_hidden_dim,
+            bn=False,
+            drop=False,
+            nl=nn.Tanh(),
+            img_dim=img_dim[1],
+            output_nl=nn.Sigmoid()
+        )
 
-        return action.detach()
-    
-    def evaluate(self, state, action):  
-        action_mean = self.actor(state)
+    def evaluate(self, state, action): 
+        hidden = self.shared_net(state)
+        action_mean = self.actor_head(hidden.detach())
+
+        state_hat = self.decoder(hidden)
 
         action_var = self.action_var.expand_as(action_mean)
         cov_mat = torch.diag_embed(action_var)
@@ -154,6 +156,6 @@ class ActorCriticMLP(nn.Module):
         
         action_logprobs = dist.log_prob(action)
         dist_entropy = dist.entropy()
-        state_value = self.critic(state)
-
-        return action_logprobs, torch.squeeze(state_value), dist_entropy
+        state_value = self.critic_head(hidden)
+        
+        return action_logprobs, torch.squeeze(state_value), dist_entropy, state_hat
