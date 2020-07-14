b3bd4d672d21ca9a56faa143be3d2b12404fe902
Oliver Limoyo <olimoyo@monolith>
2020-07-14 16:40:13 -0400

    Add timing to env tests

--- tests/test_env.py
+++ tests/test_env.py
@@ -1,7 +1,7 @@
 """
 Test environment with simple inputs.
 """
-import os, sys, inspect
+import os, sys, inspect, time
 currentdir = os.path.dirname(os.path.abspath(
     inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(os.path.dirname(currentdir))
@@ -34,20 +34,24 @@ def test_gym_reacher():
         p = np.random.uniform(-1,1,2)
         obs, _, _, _ = env.step(np.array([p[0], p[1]]))
         if ii % 150 == 0:
+            tic = time.time()
             env.reset()
-            print(f"Resetting with {ii} time steps")
+            reset_time = time.time() - tic
+            print(f"Resetting with {ii} time steps, time {reset_time}")
         # print("Obs received: ", obs.shape)
         # print("Action dim: ", env.action_space)
         # print("Obs dim: ", env.observation_space)
 
 def test_gym_pusher():
     env = gym.make('ThingPusher-v0')
-    for ii in range(1000):
-        print(ii)
-        obs, _, _, _ = env.step(np.array([0.1]))
-        if ii % 100 == 0:
+    for ii in range(10000000):
+        p = np.random.uniform(-1,1,2)
+        obs, _, _, _ = env.step(np.array([p[0], p[1]]))
+        if ii % 150 == 0:
+            tic = time.time()
             env.reset()
-            print("resetting")
+            reset_time = time.time() - tic
+            print(f"Resetting with {ii} time steps, time {reset_time}")
         # print("Action dim: ", env.acti
         # print("Obs received: ", obs.shapon_space)
         # print("Obs dim: ", env.observation_space)
@@ -58,8 +62,10 @@ def test_gym_visual_reacher():
         p = np.random.uniform(-1,1,2)
         obs, _, _, _ = env.step(np.array([p[0], p[1]]))
         if ii % 150 == 0:
+            tic = time.time()
             env.reset()
-            print("Resetting with {} time steps".format(ii))
+            reset_time = time.time() - tic
+            print(f"Resetting with {ii} time steps, time {reset_time}")
         # print("Action dim: ", env.action_space)
         # print("Obs dim: ", env.observation_space)
 
@@ -68,14 +74,16 @@ def test_gym_visual_pusher():
     for ii in range(10000000):
         p = np.random.uniform(-1,1,2)
         obs, _, _, _ = env.step(np.array([p[0], p[1]]))
-        if (ii + 1) % 150 == 0:
+        if ii % 150 == 0:
+            tic = time.time()
             env.reset()
-            print("Resetting with {} time steps".format(ii))
+            reset_time = time.time() - tic
+            print(f"Resetting with {ii} time steps, time {reset_time}")
         # print("Action dim: ", env.action_space)
         # print("Obs dim: ", env.observation_space)
 
 if __name__=="__main__":
-    test_gym_reacher()
+    # test_gym_reacher()
     # test_gym_pusher()
-    # test_gym_visual_reacher()
+    test_gym_visual_reacher()
     # test_gym_visual_pusher()
