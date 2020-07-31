2fdadf2a171e72ffa744a1df76dfb0d45b7822ce
Oliver Limoyo <oliver.limoyo@mail.mcgill.ca>
2020-07-31 17:13:09 -0400

    Add parallel env test

--- experiments/collect_datasets.py
+++ experiments/collect_datasets.py
@@ -23,7 +23,7 @@ def write_file_pkl(data, name, location="."):
 def visual_haptic_2D_osc():
     n_steps = 32
     env = ThingVisualPusher(
-        is_render=True,
+        is_render=False,
         render_w=64, 
         render_h=64, 
         goal_vis=False, 
@@ -32,27 +32,28 @@ def visual_haptic_2D_osc():
     )
     config = env.get_config()
 
-    repeat = 3
+    repeat = 32
 
     mag_list = list(np.arange(1.0, 1.96, 0.03)) * repeat
     n_mag = len(mag_list)
 
-    n = n_mag
+    print(f"Collecting {n_mag} trajectories")
+
     ll = 16
     ii = 0
 
     data = {
-        "img": np.zeros((n, ll, 64, 64, 3), dtype=np.uint8), 
-        "ft": np.zeros((n, ll, n_steps, 6)), 
-        "arm": np.zeros((n, ll, n_steps, 6)),
-        "action": np.zeros((n, ll, 2)), 
+        "img": np.zeros((n_mag, ll, 64, 64, 3), dtype=np.uint8), 
+        "ft": np.zeros((n_mag, ll, n_steps, 6)), 
+        "arm": np.zeros((n_mag, ll, n_steps, 6)),
+        "action": np.zeros((n_mag, ll, 2)), 
         "config": config
     }
     
     for _, m in enumerate(mag_list):
         env.reset()
         for jj in range(ll): 
-            u_x = 0.30 * m
+            u_x = 0.35 * m
             u_y = np.clip(np.random.normal(0, 0.25), -0.30, 0.30) 
             obs, reward, done, info = env.step(action=np.array([u_x, u_y]))
             data["img"][ii, jj] = obs["img"]
@@ -78,11 +79,12 @@ def visual_haptic_2D():
     mag_list = list(np.arange(1.0, 1.96, 0.03))  
     n_mag = len(mag_list)
 
-    init_pos_list = list(3 * np.arange(-0.050, 0.060, 0.005))
+    init_pos_list = list(np.arange(-0.050, 0.060, 0.0025))
     n_init = len(init_pos_list)
     
     n = n_init * n_mag
-    ll = 10
+    print(f"Collectin {n} trajectories")
+    ll = 16
     ii = 0
 
     data = {
@@ -99,7 +101,7 @@ def visual_haptic_2D():
             for _ in range(5):
                 env.step(action=np.array([0, h]))
             for jj in range(ll): 
-                u = np.array([0.50 * m, 0])
+                u = np.array([0.35 * m, 0])
                 obs, reward, done, info = env.step(action=u)
                 data["img"][ii, jj] = obs["img"]
                 data["ft"][ii, jj] = obs["ft"]
@@ -149,4 +151,4 @@ if __name__ == "__main__":
     # data = visual_haptic_2D()
     data = visual_haptic_2D_osc()
 
-    write_file_pkl(data=data, name="visual_haptic_2D_osc", location="./data/datasets/")
+    write_file_pkl(data=data, name="visual_haptic_2D_osc_len16", location="./data/datasets/")
