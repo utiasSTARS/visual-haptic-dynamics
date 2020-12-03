Fix and use custom KL to avoid memory bug

tests/test_kl_divergence.py
@@ -6,28 +6,24 @@ import os, sys, time
 os.sys.path.insert(0, "..")
 
 import torch
-from losses import kl
+from losses import kl, torch_kl
 
 def test_KL(bs=1000):
-    mu0 = -torch.rand((bs,3))
-    var0 = 3*torch.eye(3).repeat(bs,1,1)
-    mu1 = torch.rand((bs,3))
-    var1 = 2.5*torch.eye(3).repeat(bs,1,1)
-    
+    mu0 = -torch.rand((16,bs,3))
+    var0 = 3*torch.eye(3).repeat(16,bs,1,1)
+    mu1 = torch.rand((16,bs,3))
+    var1 = 2.5*torch.eye(3).repeat(16,bs,1,1)
+
     # Test Pytorch's KL divergence
     tic = time.time()
-    p = torch.distributions.MultivariateNormal(mu0, var0)
-    q = torch.distributions.MultivariateNormal(mu1, var1)
-    kl_loss_torch = torch.distributions.kl_divergence(p, q)
+    kl_loss_torch = torch_kl(mu0=mu0, cov0=var0, mu1=mu1, cov1=var1)
     toc = time.time() - tic
-
     print(f"Torch distribution package: {kl_loss_torch}, Time Taken: {toc}")
     
     # Test custom KL
     tic = time.time()
     kl_loss_custom = kl(mu0=mu0, cov0=var0, mu1=mu1, cov1=var1)
     toc = time.time() - tic
-
     print(f"Custom: {kl_loss_custom}, Time Taken: {toc}")
 
     mse = torch.sum(kl_loss_torch - kl_loss_custom)**2

train2.py
@@ -38,7 +38,7 @@ from models import (
     ProductOfExperts
 )
 from datasets import VisualHaptic, ImgCached
-from losses import torch_kl
+from losses import torch_kl, kl
 
 def setup_opt_iter(args):
     # Loss functions
@@ -216,7 +216,7 @@ def setup_opt_iter(args):
             )
             p_z = {"z": z_t1_hat, "mu": mu_z_t1_hat, "cov": var_z_t1_hat}
             
-            loss_kl += torch_kl(
+            loss_kl += kl(
                 mu0=q_z["mu"],
                 cov0=q_z["cov"],
                 mu1=torch.cat((mu_z_i, p_z["mu"]), axis=0),
@@ -268,7 +268,7 @@ def setup_opt_iter(args):
                     u_nstep = u_nstep.reshape(l_nstep, n_nstep, *u_nstep.shape[1:])
                     h_t = h_t.reshape(l_nstep, n_nstep, *h_t.shape[1:])
 
-                    loss_kl += torch_kl(
+                    loss_kl += kl(
                         mu0=q_z_nstep["mu"],
                         cov0=q_z_nstep["cov"],
                         mu1=p_z_nstep["mu"],
@@ -333,7 +333,7 @@ def train(args):
 
     slurm_id = os.environ.get('SLURM_JOB_ID')
     if slurm_id is not None:
-        save_dir = save_dir + "_" + slurm_id
+        save_dir = slurm_id + "_" + save_dir
         user = os.environ.get('USER')
         checkpoint_dir = f"/checkpoint/{user}/{slurm_id}"
     else: